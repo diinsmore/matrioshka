8e7f7a52a2cb98cc862811a51be3dd3a91fdbb3e
diinsmore <mayadinsmore11804@gmail.com>
2025-04-28 19:24:28 -0400

    moved the source files into a dedicated folder

---
+++ code/graphics_engine.py
@@ -0,0 +1,148 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from camera import Camera
+    from ui import UI
+    from procgen import ProcGen
+    from sprite_manager import SpriteManager
+    from chunk_manager import ChunkManager
+    import numpy as np
+    
+import pygame as pg
+from os import walk
+from os.path import join
+import math
+
+from settings import *
+from weather import Weather
+
+class GraphicsEngine:
+    def __init__(
+        self, 
+        screen: pg.Surface,
+        camera: Camera,
+        graphics: dict[str, list[pg.Surface]],
+        ui: UI,
+        proc_gen: ProcGen, 
+        sprite_manager: SpriteManager,
+        chunk_manager: ChunkManager
+    ):
+        self.screen = screen
+        self.camera = camera
+        self.camera_offset = camera.offset
+        self.graphics = graphics
+        self.ui = ui
+        self.proc_gen = proc_gen
+        self.sprite_manager = sprite_manager
+        self.chunk_manager = chunk_manager
+
+        self.tile_map = proc_gen.tile_map
+        self.tile_data = proc_gen.tile_data
+        self.biome_order = proc_gen.biome_order
+
+        self.all_sprites = sprite_manager.all_sprites
+        self.cloud_sprites = sprite_manager.cloud_sprites
+        
+        self.weather = Weather(screen)
+     
+    # world
+    def render_bg_images(self, bg_type: str) -> None:
+        '''render the current biome's landscape & underground graphics''' 
+        if bg_type != 'terrain wall':
+            image = self.graphics[self.proc_gen.current_biome][bg_type]
+        else:
+            image = self.graphics['walls'][' '.join([self.get_terrain_type(), 'wall'])]
+
+        elev_data = self.get_elevation_data()
+
+        for x in range((BIOME_WIDTH * TILE_SIZE) // image.get_width()): 
+            left = (image.get_width() * x) + self.get_biome_offset()
+            if bg_type == 'landscape': # has a static y-axis
+                top = elev_data['landscape base'] - image.get_height()
+                self.screen.blit(image, (left, top) - self.camera_offset)
+            else: 
+                # underground graphics
+                for y in range(elev_data['underground span'] // image.get_height() + 1): 
+                    top = elev_data['underground start' if bg_type == 'underground' else 'landscape base']
+                    top += image.get_height() * y
+                    self.screen.blit(image, (left, top) - self.camera_offset)
+
+    def get_terrain_type(self) -> str:
+        '''just for getting a specific wall variant but could become more modular'''
+        match self.proc_gen.current_biome:
+            case 'highlands':
+                return 'stone'
+
+            case 'forest' | 'taiga' | 'tundra':
+                return 'dirt'
+
+            case 'desert':
+                return 'sandstone'
+                
+            case 'underworld':
+                return 'magma'
+
+    def get_elevation_data(self) -> dict[str, int]:
+        elev_params = BIOMES[self.proc_gen.current_biome]['elevation']
+        elev_data = {
+            'range': (elev_params['bottom'] - elev_params['top']) * TILE_SIZE,
+            'underground span': (MAP_SIZE[1] * TILE_SIZE) - (elev_params['bottom'] * TILE_SIZE), # minimum number of tiles from the bottom of the map to the surface
+            'underground start': elev_params['bottom'] * TILE_SIZE 
+        }
+        # approximate midpoint between the biome's highest/lowest elevation
+        elev_data['landscape base'] = elev_data['underground start'] - (elev_data['range'] // 2) 
+        return elev_data
+
+    def get_biome_offset(self) -> int:
+        '''return the current biome's distance (in pixels) from the left edge of the screen'''
+        return self.proc_gen.biome_order[self.proc_gen.current_biome] * (BIOME_WIDTH * TILE_SIZE)
+
+    def render_tiles(self) -> None:
+        visible_chunks = self.chunk_manager.update()
+        for coords in visible_chunks: # all visible tile coordinates
+            for (x, y) in coords: # individual tile coordinates
+                # ensure that the tile is within the map borders & is a solid tile
+                if 0 <= x < MAP_SIZE[0] and 0 <= y < MAP_SIZE[1] \
+                and self.tile_map[x, y] != self.tile_data['air']['id']:
+                    # match the tile to its graphic
+                    for tile in self.tile_data.keys():
+                        if self.tile_data[tile]['id'] == self.tile_map[x, y]:
+                            # convert from tile to pixel coordinates
+                            px_x = (x * TILE_SIZE) - self.camera_offset.x
+                            px_y = (y * TILE_SIZE) - self.camera_offset.y 
+                            self.screen.blit(self.tile_data[tile]['graphic'], (px_x, px_y))
+
+
+    # sprites
+    def animate(self, sprite: pg.sprite.Sprite, dt: float) -> None:
+        if sprite.state != 'idle' and sprite.direction.x:
+            sprite.frame_index += sprite.animation_speed * dt
+            flip_x = False
+            if sprite.facing_left:
+                if sprite.direction.x > 0:
+                    flip_x = True
+            else:
+                if sprite.direction.x < 0:
+                    flip_x = True
+            sprite.image = pg.transform.flip(
+                sprite.frames[sprite.state][int(sprite.frame_index % len(sprite.frames[sprite.state]))],
+                flip_x,
+                False
+            ) 
+        
+    def render_sprites(self) -> None:
+        for sprite in sorted(self.all_sprites, key = lambda sprite: sprite.z): # layer graphics by their z-level
+            self.screen.blit(sprite.image, sprite.rect.topleft - self.camera_offset)
+
+    def update(self, mouse_coords, left_click, dt) -> None:
+        self.sprite_manager.update(dt)
+        self.weather.update()
+
+        for bg in ('landscape', 'terrain wall', 'underground'):
+            self.render_bg_images(bg)
+        self.render_tiles()
+        self.render_sprites()
+        self.ui.update(mouse_coords, left_click)
+
+        for sprite in self.sprite_manager.all_sprites:
+            self.animate(sprite, dt)
---
+++ code/procgen.py
@@ -0,0 +1,191 @@
+import pygame as pg
+import numpy as np
+import noise
+import random
+
+from settings import TILES, TILE_SIZE, MAP_SIZE, CELL_SIZE, BIOMES, BIOME_WIDTH
+from sprites import *
+from timer import Timer
+
+# TODO: refine the ore distribution to generate clusters of a particular gemstone rather than randomized for each tile 
+
+class ProcGen:
+    def __init__(self):
+        self.tile_map = np.zeros((MAP_SIZE[0], MAP_SIZE[1]), dtype = np.uint8)
+        self.height_map = np.zeros(MAP_SIZE[0], dtype = np.float32)
+        self.biome_order = self.order_biomes()
+        self.current_biome = 'forest'
+        
+        self.tile_data = self.get_tile_data()
+        self.generate_terrain()
+    
+    def get_tile_data(self) -> dict[str, int]:
+        # give each tile a unique number to store in the tile map when it appears
+        id_data = {}
+        for index, tile in enumerate(TILES):
+            id_data[tile] = {'id': index} 
+        
+        return id_data
+    
+    # TODO: randomize the sequence
+    def order_biomes(self) -> dict[str, int]:
+        order = {}
+        # excluding the underworld since it spans the entire map width
+        biomes = [b for b in BIOMES.keys() if b != 'underworld']
+        for index, biome in enumerate(biomes):
+            order[biome] = index
+
+        return order
+
+    def generate_height_map(self) -> None:
+        '''generates a height map for every biome using 1d perlin noise'''
+        seed = 2285 # TODO: add the option to enter a custom seed
+        # TODO: add more gradual transitions between biomes
+        for biome, order_num in self.biome_order.items(): 
+            elev_data = BIOMES[biome]['elevation']
+            elev_range = elev_data['bottom'] - elev_data['top']
+            
+            noise_params = BIOMES[biome]['noise params']
+            
+            for x in range(order_num * BIOME_WIDTH, (order_num + 1) * BIOME_WIDTH):
+                # generates a random float between -1/1
+                num = noise.pnoise1(  
+                    x / noise_params['scale'], 
+                    octaves = noise_params['octaves'], 
+                    persistence = noise_params['persistence'],
+                    lacunarity = noise_params['lacunarity'],
+                    repeat = -1, # don't repeat
+                    base = seed
+                )
+                # convert the range from -1/1 to 0/1 to keep values positive (or zero, but rarely)
+                range01 = (num + 1) / 2 
+                self.height_map[x] = elev_data['top'] + range01 * elev_range
+
+    def generate_terrain(self) -> None:
+        self.generate_height_map()
+        # TODO: add the underworld
+        for x in range(MAP_SIZE[0]):
+            surface_level = round(self.height_map[x])
+            for y in range(MAP_SIZE[1]):
+                if y < surface_level: 
+                    self.tile_map[x, y] = self.tile_data['air']['id']
+
+                elif y == surface_level: 
+                    self.tile_map[x, y] = self.tile_data['dirt']['id']
+                    
+                else:
+                    # calculate the tile's depth relative to the height of the map
+                    rel_depth = (y - surface_level) / MAP_SIZE[1]
+                    if rel_depth < 0.1:
+                        self.tile_map[x, y] = self.tile_data['dirt']['id']
+
+                    elif rel_depth < 0.2:
+                        self.tile_map[x, y] = self.tile_data['stone' if random.randint(0, 100) <= 33 else 'dirt']['id']
+
+                    elif rel_depth < 0.4:
+                        if random.randint(0, 100) <= 25:
+                            self.tile_map[x, y] = random.choice((self.tile_data['sandstone']['id'], self.tile_data['ice']['id']))
+                        else:
+                            self.tile_map[x, y] = self.tile_data['stone' if random.randint(0, 100) < 60 else 'dirt']['id'] 
+                    else:
+                        self.ore_distribution(x, y, self.current_biome)  
+    
+    def ore_distribution(self, x: int, y: int, biome: str) -> None:
+        '''
+        Distribute ore tiles based on the biome's probability of containing such a tile.
+        If no ore is selected, fill the space with a tile common to the biome.
+        '''
+        ores = [tile for tile in self.tile_data if self.tile_data['copper']['id'] <= self.tile_data[tile]['id'] <= self.tile_data['gold']['id']]
+        
+        non_ores = [tile for tile in self.tile_data if self.tile_data['dirt']['id'] <= self.tile_data[tile]['id'] <= self.tile_data[
+                    'obsidian' if biome != 'underworld' else 'hellstone']['id']]
+
+        if random.random() < 0.1:
+            ore_selected = self.calc_tile_prob(ores, x, y, biome)
+            if not ore_selected:
+                # select a random non-ore tile
+                tile = random.choice(non_ores)
+                self.tile_map[x, y] = self.tile_data[tile]['id']
+        else:
+            tile = random.choice(non_ores)
+            self.tile_map[x, y] = self.tile_data[tile]['id']
+                        
+    def calc_tile_prob(self, tiles: list[str], x: int, y: int, biome: str) -> bool:
+        '''randomly determine which tile (if any) should be placed at the given coordinate'''
+        tiles = sorted(tiles, key = lambda tile: BIOMES[biome]['tile probs'][tile], reverse=True)
+        for index, tile in enumerate(tiles):
+            if random.randint(0, 10) <= BIOMES[biome]['tile probs'][tile]:
+                self.tile_map[x, y] = self.tile_data[tile]['id']
+                return True
+            else:
+                if index < len(tiles) - 1:
+                    continue
+                return False
+
+    def add_tree(self, coords: tuple[int, int], biome) -> None:
+        image = load_image(join('..', 'graphics', 'terrain', 'trees', f'{biome} tree.png'))
+        if TILE_SIZE < coords[0] < MAP_SIZE[0] - TILE_SIZE: # not positioned at the edge of the map
+            center_tile = self.tile_map[coords[0] + 1, coords[1]]    
+            left_tile = self.tile_map[coords[0] - 1, coords[1]]
+            right_tile = self.tile_map[coords[0] + 1, coords[1]]
+            # only spawn at coordinates where the x-axis is consistent for 3+ tiles (equal to the tree's width of 36px)
+            if center_tile == 0 and left_tile == 0 and right_tile == 0: # dirt tiles
+                Tree(
+                    coords = (coords[0] * TILE_SIZE, coords[1] * TILE_SIZE - image.get_height()), 
+                    image = image, 
+                    z = Z_LAYERS['bg'], 
+                    groups = self.all_sprites
+                )
+
+    def get_player_spawn_point(self) -> tuple[int, int]:
+            '''spawn at the nearest flat surface (at least 3 solid tiles on the same y-axis) to the map's center-x'''
+            center_x = MAP_SIZE[0] // 2
+            y = int(self.height_map[center_x])
+            # check for neighboring tiles being on the same y-axis and only having air tiles above them
+            if self.valid_spawn_point(center_x, y): 
+                return (center_x * TILE_SIZE, y * TILE_SIZE)
+            else:
+                valid_coords = []
+                for x in range(1, MAP_SIZE[0] - 1):
+                    xy = (x, int(self.height_map[x]))
+                    if self.valid_spawn_point(xy[0], xy[1]):
+                        valid_coords.append(xy)
+
+                if valid_coords:
+                    # select the coordinate nearest to the map's center-x
+                    spawn_point = min(valid_coords, key = lambda coord: abs(coord[0] - center_x))
+                    return (spawn_point[0] * TILE_SIZE, spawn_point[1] * TILE_SIZE)
+                else:
+                    # default to the center-x
+                    return (center_x * TILE_SIZE, y * TILE_SIZE)
+
+    def valid_spawn_point(self, x: int, y: int) -> bool:
+        '''
+        Determine whether a 3x3 horizontal line of tiles are all solid, 
+        to find a suitable spawn point for the player (and maybe future entities)
+        '''
+        current_tile = self.tile_map[x, y]
+        left_tile = self.tile_map[x - 1, y]
+        right_tile = self.tile_map[x + 1, y]
+
+        topleft_tile = self.tile_map[x - 1, y - 1]
+        topcenter_tile = self.tile_map[x, y - 1]
+        topright_tile = self.tile_map[x + 1, y - 1]
+        
+        air = self.tile_data['air']['id']
+        return all(tile != air for tile in (current_tile, left_tile, right_tile)) and \
+               all(tile == air for tile in (topleft_tile, topcenter_tile, topright_tile))
+
+    # update tiles that have been mined, will also have to account for the use of explosives and perhaps weather altering the terrain
+    def update_map(self, tile_coords: tuple[int, int], collision_map: dict[tuple[int, int], pg.Rect]) -> None:
+        cell_coords = (
+            tile_coords[0] // CELL_SIZE, 
+            tile_coords[1] // CELL_SIZE
+        )
+        if cell_coords in collision_map: # false if you're up in the stratosphere
+            rect = pg.Rect(tile_coords[0] * TILE_SIZE, tile_coords[1] * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+            if rect in collision_map[cell_coords]:
+                # sprites could occasionally pass through tiles whose graphic was still being rendered
+                # removing the associated rectangle only after the tile id's update is confirmed appears to fix the issue
+                if self.tile_map[tile_coords[0], tile_coords[1]] == self.tile_data['air']['id']:
+                    collision_map[cell_coords].remove(rect)
