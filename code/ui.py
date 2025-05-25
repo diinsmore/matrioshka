e58eae586e00784e378fe849c5bfd00cdd59b7e9
diinsmore <mayadinsmore11804@gmail.com>
2025-05-24 23:05:38 -0400

    fixed a bug where just hovering the mouse over an item would craft it if you had the requisite materials

--- code/ui.py
+++ code/ui.py
@@ -135,7 +135,7 @@ class UI:
         self.mouse_grid.update(mouse_coords, mouse_moving, click_states)
         self.HUD.update()
         self.mini_map.update()
-        self.craft_window.update(mouse_coords, click_states) # keep above the inventory ui otherwise item names may be rendered behind the window
+        self.craft_window.update(mouse_coords, click_states['left']) # keep above the inventory ui otherwise item names may be rendered behind the window
         self.inventory_ui.update(click_states, mouse_coords, update_collision_map)
         
 
