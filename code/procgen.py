32978476097c23fec34eb2c01da3757bb963f363
diinsmore <mayadinsmore11804@gmail.com>
2025-06-30 21:30:23 -0400

    refining the biome transitions

--- code/procgen.py
+++ code/procgen.py
@@ -28,7 +28,7 @@ class ProcGen:
             self.height_map = self.terrain_gen.height_map
             self.cave_map = self.terrain_gen.cave_map
 
-            self.tree_gen = TreeGen(self.tile_map, self.tile_IDs, self.height_map, self.valid_spawn_point)
+            self.tree_gen = TreeGen(self.tile_map, self.tile_IDs, self.height_map, self.valid_spawn_point, self.biome_order)
             self.tree_map = self.tree_gen.tree_map
 
             self.gen_world()
@@ -55,30 +55,30 @@ class ProcGen:
     @staticmethod
     def order_biomes() -> dict[str, int]:
         # TODO: randomize this sequence
-        order = {'tundra': 0, 'taiga': 1, 'forest': 2, 'desert': 3, 'highlands': 4}
+        order = {'highlands': 0, 'desert': 1, 'forest': 2, 'taiga': 3, 'tundra': 4}
         return order
 
     def get_player_spawn_point(self) -> tuple[int, int]:
-            '''spawn at the nearest flat surface (at least 3 solid tiles on the same y-axis) to the map's center-x'''
-            center_x = MAP_SIZE[0] // 2
-            y = int(self.height_map[center_x])
-            # check for neighboring tiles being on the same y-axis and only having air tiles above them
-            if self.valid_spawn_point(center_x, y): 
-                return (center_x * TILE_SIZE, y * TILE_SIZE)
-            else:
-                valid_coords = []
-                for x in range(1, MAP_SIZE[0] - 1):
-                    xy = (x, int(self.height_map[x]))
-                    if self.valid_spawn_point(*xy):
-                        valid_coords.append(xy)
+        '''spawn at the nearest flat surface (at least 3 solid tiles on the same y-axis) to the map's center-x'''
+        center_x = MAP_SIZE[0] // 2
+        y = int(self.height_map[center_x])
+        # check for neighboring tiles being on the same y-axis and only having air tiles above them
+        if self.valid_spawn_point(center_x, y): 
+            return (center_x * TILE_SIZE, y * TILE_SIZE)
+        else:
+            valid_coords = []
+            for x in range(1, MAP_SIZE[0] - 1):
+                xy = (x, int(self.height_map[x]))
+                if self.valid_spawn_point(*xy):
+                    valid_coords.append(xy)
 
-                if valid_coords:
-                    # select the coordinate nearest to the map's center-x
-                    spawn_point = min(valid_coords, key = lambda coord: abs(coord[0] - center_x))
-                    return (spawn_point[0] * TILE_SIZE, spawn_point[1] * TILE_SIZE)
-                else:
-                    # default to the center-x
-                    return (center_x * TILE_SIZE, y * TILE_SIZE)
+            if valid_coords:
+                # select the coordinate nearest to the map's center-x
+                spawn_point = min(valid_coords, key = lambda coord: abs(coord[0] - center_x))
+                return (spawn_point[0] * TILE_SIZE, spawn_point[1] * TILE_SIZE)
+            else:
+                # default to the center-x
+                return (center_x * TILE_SIZE, y * TILE_SIZE)
 
     def valid_spawn_point(self, x: int, y: int) -> bool:
         left_tile = self.tile_map[x - 1, y]
@@ -103,32 +103,60 @@ class TerrainGen:
         self.tile_IDs = tile_IDs
         self.biome_order = biome_order
         self.current_biome = current_biome
-
+        
         self.seed = 2285 # TODO: add the option to enter a custom seed
         self.tile_map = np.zeros((MAP_SIZE[0], MAP_SIZE[1]), dtype = np.uint8)
         self.height_map = self.gen_height_map()
         self.cave_gen = CaveGen(self.tile_map, self.height_map, self.seed)
         self.cave_map = self.cave_gen.map
-
+        
     def gen_height_map(self) -> np.ndarray:
         '''generates a height map for every biome using 1d perlin noise'''
-        height_map = np.zeros(MAP_SIZE[0], dtype = np.float32) 
-        # TODO: add more gradual transitions between biomes
-        for biome, order_num in self.biome_order.items(): 
-            elev_data = BIOMES[biome]['elevation']
-            elev_range = elev_data['bottom'] - elev_data['top']
-            params = BIOMES[biome]['height map']
-            for x in range(order_num * BIOME_WIDTH, (order_num + 1) * BIOME_WIDTH):
-                n = noise.pnoise1(  
-                        x / params['scale'], 
-                        octaves = params['octaves'], 
-                        persistence = params['persistence'],
-                        lacunarity = params['lacunarity'],
-                        repeat = -1, # don't repeat
-                        base = self.seed,
-                    )
-                height_map[x] = elev_data['top'] + n * elev_range
+        height_map = np.zeros(MAP_SIZE[0], dtype = np.float32)
+        lerp_range = BIOME_WIDTH // 10 # how far to extend the linear interpolation on noise parameters when generating biome transitions
+        biome_names = list(self.biome_order.keys())
+        second_to_last_idx = len(biome_names) - 1
+        for idx, biome in enumerate(biome_names):
+            start = idx * BIOME_WIDTH
+            end = start + BIOME_WIDTH
+            map_slice = np.arange(start, end) # represents the section of the height map local to the current biome
+            elevs = self.get_biome_elevations(map_slice, biome)
+            
+            if idx < second_to_last_idx: # another biome lies to the right
+                next_biome_elevs = self.get_biome_elevations(map_slice, biome_names[idx + 1])
+            else:
+                next_biome_elevs = None
+
+            for biome_x, world_x in enumerate(map_slice):
+                if biome_x < BIOME_WIDTH - lerp_range or next_biome_elevs is None: # outside the transition zone
+                    height_map[world_x] = elevs[biome_x]
+                else:
+                    rel_pos = (biome_x - (BIOME_WIDTH - lerp_range)) / lerp_range # what % of the way x is to the end of the biome transition zone
+                    height_map[world_x] = ((1 - rel_pos) * elevs[biome_x]) + (rel_pos * next_biome_elevs[biome_x]) # lerp for a smoother biome transition
+                    
         return height_map
+
+    def get_biome_elevations(self, map_slice: np.ndarray, biome: str) -> np.ndarray:
+        biome_data = BIOMES[biome]
+        noise_params = biome_data['height map']
+        elev_params = biome_data['elevation']
+        top, bottom = elev_params['top'], elev_params['bottom']
+        elev_range = bottom - top
+
+        n = np.array([
+            noise.pnoise1(
+                x / noise_params['scale'], 
+                noise_params['octaves'], 
+                noise_params['persistence'], 
+                noise_params['lacunarity'], 
+                repeat = -1, 
+                base = self.seed
+            ) 
+            for x in map_slice
+        ], dtype = np.float32)
+
+        half_elev = elev_range // 2
+        return top + half_elev + (n * half_elev)
 
     def place_tiles(self) -> None:
         for x in range(MAP_SIZE[0]):
@@ -136,17 +164,22 @@ class TerrainGen:
             for y in range(MAP_SIZE[1]): 
                 if y < surface_level: 
                     self.tile_map[x, y] = self.tile_IDs['air'] 
+
                 elif y == surface_level:
                     self.tile_map[x, y] = self.tile_IDs['dirt']
+
                 else:
                     if self.cave_map[x, y]:
                         self.tile_map[x, y] = self.tile_IDs['air']
+                    
                     else:
                         rel_depth = (y - surface_level) / MAP_SIZE[1] # calculate the tile's depth relative to the height of the map
                         if rel_depth < 0.1:
                             self.tile_map[x, y] = self.tile_IDs['dirt']
+                        
                         elif rel_depth < 0.2:
                             self.tile_map[x, y] = self.tile_IDs['stone' if randint(0, 100) <= 33 else 'dirt'] 
+                        
                         elif rel_depth < 0.4:
                             if randint(0, 100) <= 25:
                                 self.tile_map[x, y] = choice((self.tile_IDs['sandstone'], self.tile_IDs['ice']))
@@ -219,16 +252,25 @@ class CaveGen:
                 cave_map[x, y] = (n + 1) / 2 > params['threshold'] # convert to a range of 0-1 before comparing
         return cave_map
 
+
 class TreeGen:
-    def __init__(self, tile_map: np.ndarray, tile_IDs: dict[str, int], height_map: np.ndarray, valid_spawn_point: callable):
+    def __init__(
+        self, 
+        tile_map: np.ndarray, 
+        tile_IDs: dict[str, int], 
+        height_map: np.ndarray, 
+        valid_spawn_point: callable, 
+        biome_order: dict[str, int]
+    ):
         self.tile_map = tile_map
         self.tile_IDs = tile_IDs
         self.height_map = height_map
         self.valid_spawn_point = valid_spawn_point
+        self.biome_order = biome_order
 
         self.tree_map = set()
-        self.biomes_covered = [{'idx': idx, 'name': name} for idx, name in enumerate(BIOMES.keys()) if 'tree coverage' in BIOMES[name].keys()]
-
+        self.biomes_covered = [{'name': name, 'idx': idx} for name, idx in self.biome_order.items() if 'tree coverage' in BIOMES[name].keys()]
+        
     def get_tree_locations(self) -> None:
         for data in self.biomes_covered:
             start_x = data['idx'] * BIOME_WIDTH
--- code/settings.py
+++ code/settings.py
@@ -4,14 +4,14 @@ FPS = 60
 TILE_SIZE = 16
 CHUNK_SIZE = 24
 CELL_SIZE = 10
-
-MAP_SIZE = (2500, 200) 
+# TODO: any value larger on the x-axis will cause a glitch where the physics engine doesn't detect the player's initial contact with the ground in time and ends up spawning in a cave
+MAP_SIZE = (1590, 200) 
 
 BIOMES = {
     'highlands': {
         'height map': {'scale': 325, 'octaves': 5, 'persistence': 1.6, 'lacunarity': 2.1},
         'cave map': {'scale': 30.0, 'octaves': 5, 'persistence': 2.0, 'lacunarity': 2.3, 'threshold': 0.4},
-        'elevation': {'top': 30, 'bottom': 130}, 
+        'elevation': {'top': 0, 'bottom': 70}, 
         'tile probs': {'obsidian': 2, 'coal': 5, 'copper': 7, 'iron': 8, 'silver': 8, 'gold': 5},
         'liquid probs': {'water': 3, 'lava': 5},
     }, 
@@ -19,16 +19,16 @@ BIOMES = {
     'desert': {
         'height map': {'scale': 400, 'octaves': 4, 'persistence': 0.9, 'lacunarity': 1.4},
         'cave map': {'scale': 60.0, 'octaves': 3, 'persistence': 0.7, 'lacunarity': 0.9, 'threshold': 0.6},
-        'elevation': {'top': 130, 'bottom': 180},
+        'elevation': {'top': 50, 'bottom': 90},
         'tile probs': {'coal': 3, 'copper': 7, 'iron': 6, 'silver': 6, 'gold': 2},
         'liquid probs': {'oil': 7, 'lava': 5},
         
     },
     
     'forest': {
-        'height map': {'scale': 450, 'octaves': 3, 'persistence': 1.2, 'lacunarity': 2.0},
+        'height map': {'scale': 400, 'octaves': 3, 'persistence': 1.2, 'lacunarity': 2.0},
         'cave map': {'scale': 30.0, 'octaves': 4, 'persistence': 1.6, 'lacunarity': 1.3, 'threshold': 0.55},
-        'elevation': {'top': 150, 'bottom': 190},
+        'elevation': {'top': 70, 'bottom': 110},
         'tile probs': {'coal': 7, 'copper': 4, 'iron': 5, 'silver': 3, 'gold': 3},
         'liquid probs': {'water': 7, 'lava': 2},
         'tree coverage': 40
@@ -37,16 +37,16 @@ BIOMES = {
     'taiga': {
         'height map': {'scale': 400, 'octaves': 4, 'persistence': 1.3, 'lacunarity': 1.6},
         'cave map': {'scale': 40.0, 'octaves': 4, 'persistence': 1.6, 'lacunarity': 1.9, 'threshold': 0.4},
-        'elevation': {'top': 110, 'bottom': 160},
+        'elevation': {'top': 35, 'bottom': 90},
         'tile probs': {'coal': 5, 'copper': 3, 'iron': 6, 'silver': 5, 'gold': 4},
         'liquid probs': {'water': 5, 'lava': 1},
         'tree coverage': 30
     },
     
     'tundra': {
-        'height map': {'scale': 500, 'octaves': 3, 'persistence': 0.8, 'lacunarity': 1.3},
+        'height map': {'scale': 450, 'octaves': 3, 'persistence': 1.2, 'lacunarity': 1.5},
         'cave map': {'scale': 70.0, 'octaves': 2, 'persistence': 0.6, 'lacunarity': 1.8, 'threshold': 0.35},
-        'elevation': {'top': 170, 'bottom': 200},
+        'elevation': {'top': 90, 'bottom': 125},
         'tile probs': {'coal': 4, 'copper': 6, 'iron': 5, 'silver': 6, 'gold': 3},
         'liquid probs': {'water': 5, 'oil': 7},
     }, 
@@ -54,7 +54,7 @@ BIOMES = {
     'underworld': {
         'height map': {'scale': 300, 'octaves': 6, 'persistence': 1.7, 'lacunarity': 2.0},
         'cave map': {'scale': 90.0, 'octaves': 6, 'persistence': 2.5, 'lacunarity': 2.4, 'threshold': 0.3},
-        'elevation': {'top': 400, 'bottom': MAP_SIZE[1]},
+        'elevation': {'top': 160, 'bottom': MAP_SIZE[1]},
         'tile probs': {'hellstone': 7, 'obsidian': 6, 'coal': 8, 'copper': 8, 'iron': 8, 'silver': 5, 'gold': 4},
         'liquid probs': {'oil': 6, 'lava': 9},
     },
