4d226caf8d2106523eebd04b56e9b9d7b8b1539d
diinsmore <mayadinsmore11804@gmail.com>
2025-06-30 21:59:10 -0400

    Update camera.py

--- code/camera.py
+++ code/camera.py
@@ -7,7 +7,11 @@ class Camera:
         self.coords = pg.Vector2(coords)
         self.offset = pg.Vector2()
         self.update_factor = 0.05
+        self.half_screen_x, self.half_screen_y = RES[0] // 2, RES[1] // 2
+        self.max_x, self.max_y = MAP_SIZE[0] * TILE_SIZE - self.half_screen_x, MAP_SIZE[1] * TILE_SIZE - self.half_screen_y
 
     def update(self, target_coords: pg.Vector2) -> None:
         self.coords += (target_coords - self.coords) * self.update_factor
-        self.offset.x, self.offset.y = int(self.coords.x) - (RES[0] // 2), int(self.coords.y) - (RES[1] // 2)
+        self.coords.x = max(self.half_screen_x, min(self.coords.x, self.max_x))
+        self.coords.y = min(self.coords.y, self.max_y) # not adding a minimum limit until the space biome (if one is to exist) is configured 
+        self.offset.x, self.offset.y = int(self.coords.x) - self.half_screen_x, int(self.coords.y) - self.half_screen_y
