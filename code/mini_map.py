41af92c5ef7777cdc27dc59e3ec418f567aff254
diinsmore <mayadinsmore11804@gmail.com>
2025-07-26 16:06:26 -0400

    Update mini_map.py

--- code/mini_map.py
+++ code/mini_map.py
@@ -7,7 +7,7 @@ class MiniMap:
     def __init__(
         self, 
         screen: pg.Surface,
-        camera_offset: pg.Vector2,
+        cam_offset: pg.Vector2,
         tile_map: np.ndarray, 
         tile_IDs: dict[str, int],
         tile_IDs_to_names: dict[int, str], 
@@ -15,7 +15,7 @@ class MiniMap:
         get_tile_material: callable
     ):
         self.screen = screen
-        self.camera_offset = camera_offset
+        self.cam_offset = cam_offset
         self.tile_map = tile_map
         self.tile_IDs = tile_IDs
         self.tile_IDs_to_names = tile_IDs_to_names
@@ -24,7 +24,7 @@ class MiniMap:
         
         self.visited_tiles = np.full(MAP_SIZE, False, dtype = bool)
         
-        self.tiles_x, self.tiles_y = 100, 100
+        self.tiles_x, self.tiles_y = 80, 80
         self.tile_px_w, self.tile_px_h = 2, 2
         self.outline_w = self.tiles_x * self.tile_px_w
         self.outline_h = self.tiles_y * self.tile_px_h
@@ -63,28 +63,42 @@ class MiniMap:
             pg.draw.rect(self.screen, 'black', outline1, 1)
 
     def render_tiles(self) -> None:
-        tiles = self.get_map_slice()
-        rows, cols = tiles.shape
-        for x in range(rows):
-            for y in range(cols):
+        tile_map = self.get_map_slice()
+        cols, rows = tile_map.shape
+        for x in range(cols):
+            for y in range(rows):
                 image = pg.Surface((self.tile_px_w, self.tile_px_h))
-                tile_ID = tiles[x, y]
+                tile_ID = tile_map[x, y]
                 tile_name = self.tile_IDs_to_names[tile_ID]
+                
                 if tile_name in self.non_tiles:
                     tile_color = (178, 211, 236) if tile_name == 'air' else 'black'
                 else:
                     tile_color = self.RGBs[self.get_tile_material(tile_ID)]
+
                 image.fill(tile_color)
                 rect = image.get_rect(topleft = pg.Vector2(self.padding, self.padding) + (x * self.tile_px_w, y * self.tile_px_h))
                 self.screen.blit(image, rect)
 
     def get_map_slice(self) -> np.ndarray:
-        tile_offset = self.camera_offset // TILE_SIZE
-        left_edge = max(0, int(tile_offset.x - self.border_dist_x))
-        right_edge = min(self.tile_map.shape[0], int(tile_offset.x + self.border_dist_x))
-        top_edge = max(0, int(tile_offset.y - self.border_dist_y))
-        bottom_edge = min(self.tile_map.shape[1], int(tile_offset.y + self.border_dist_y))
-        return self.tile_map[left_edge:right_edge, top_edge:bottom_edge]
+        tile_offset_x = int(self.cam_offset.x / TILE_SIZE)
+        tile_offset_y = int(self.cam_offset.y / TILE_SIZE)
+        
+        left_edge = max(0, tile_offset_x - self.border_dist_x)
+        right_edge = min(self.tile_map.shape[0], tile_offset_x + self.border_dist_x)
+        top_edge = max(0, tile_offset_y - self.border_dist_y)
+        bottom_edge = min(self.tile_map.shape[1], tile_offset_y + self.border_dist_y)
+        
+        map_slice = self.tile_map[left_edge:right_edge, top_edge:bottom_edge]
+        full_slice = np.full((self.tiles_x, self.tiles_y), self.tile_IDs['air'], dtype = np.uint8) # fill any gaps with air if the map's edge is reached
+        start_y = max(0, self.border_dist_y - (tile_offset_y - top_edge)) # not needed for the x axis since movement is constrained to within the tile map's left/right borders
+        # determine how many cols/rows represent the tile map vs empty space outside the map
+        cols, rows = map_slice.shape
+        map_cols = min(cols, self.tiles_x) 
+        map_rows = min(rows, self.tiles_y - start_y)
+        
+        full_slice[:map_cols, start_y:start_y + map_rows] = map_slice[:map_cols, :map_rows]
+        return full_slice
 
     def update(self) -> None:
         self.render_outline()
