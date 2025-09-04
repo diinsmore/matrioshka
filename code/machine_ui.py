a147dd627b3fadb452ec10cde489050bcf32f975
diinsmore <mayadinsmore11804@gmail.com>
2025-09-04 19:21:38 -0400

    updates to the machine ui structure, added icon indicating when fuel is empty

--- code/machine_ui.py
+++ code/machine_ui.py
@@ -28,12 +28,85 @@ class MachineUI:
         self.helpers = helpers
 
         self.graphics = self.assets['graphics']
+        self.icons = self.graphics['icons']
         self.render = False
+        self.empty_fuel_surf = pg.transform.scale(self.icons['empty fuel'].convert_alpha(), pg.Vector2(machine.image.get_size()) * 1.2)
+        self.empty_fuel_surf.set_colorkey((255, 255, 255))
+        self.empty_fuel_surf.set_alpha(150)
         self.highlight_color = self.assets['colors']['ui bg highlight']
         self.machine_mask = pg.mask.from_surface(machine.image)
         self.machine_mask_surf = self.machine_mask.to_surface(setcolor=(20, 20, 20, 255), unsetcolor=(0, 0, 0, 0))
 
         self.key_close_ui = self.keyboard.key_bindings['close ui window']
+
+    def check_input(self, box_data: dict[str, dict]) -> str|None:
+        box_name = None
+        for name in box_data:
+            if box_data[name]['rect'].collidepoint(self.mouse.screen_xy):
+                box_name = name
+        return box_name
+
+    def input_item(self, box_name: str, amount: int, box_data: dict[str, dict]) -> None: 
+        if self.player.item_holding in box_data['valid inputs'] and box_name != 'output': # the output box only allows input if you're adding to the item it's holding
+            if box_data['contents']['item'] is None:
+                box_data['contents']['item'] = self.player.item_holding
+
+        if self.player.item_holding == box_data['contents']['item']:
+            box_data['contents']['amount'] += amount
+            self.player.inventory.remove_item(self.player.item_holding, amount)
+
+    def extract_item(self, box_type: str, click_type: str) -> None:
+        box_inv = self.get_box_data()[box_type]['contents']
+        if box_inv['item'] is not None:
+            extract_total = box_inv['amount'] if click_type == 'left' else (box_inv['amount'] // 2)
+            box_inv['amount'] -= extract_total
+            self.player.inventory.add_item(box_inv['item'], extract_total)
+            self.player.item_holding = box_inv['item']
+            if box_inv['amount'] == 0:
+                box_inv['item'] = None
+            
+    def highlight_surf_when_hovered(self, rect_mouse_collide: bool) -> None:
+        if rect_mouse_collide:
+            self.screen.blit(self.machine_mask_surf, self.machine.rect.topleft - self.cam_offset, special_flags=pg.BLEND_RGBA_ADD)
+    
+    def render_boxes(self) -> None: 
+        data = self.get_box_data()
+        for key in data:
+            contents, rect = data[key]['contents'], data[key]['rect']
+            self.helpers.gen_bg(rect, color=self.highlight_color if rect.collidepoint(self.mouse.screen_xy) else 'black', transparent=False) 
+            self.helpers.gen_outline(rect)
+            if contents['item']: 
+                self.render_box_contents(contents, rect)          
+        
+    def render_box_contents(self, content_data: dict[str, str|int], box_rect: pg.Rect) -> None:
+        surf = self.graphics[content_data['item']]
+        self.screen.blit(surf, surf.get_frect(center=box_rect.center))
+        self.helpers.render_item_amount(content_data['amount'], box_rect.bottomright - pg.Vector2(5, 5))
+
+    def render_progress_bar(self, box: pg.Rect, progress_percent: float) -> None:
+        bar = pg.Rect(box.bottomleft, (self.box_w, self.progress_bar_h))
+        bar_outline_w = 1
+        pg.draw.rect(self.screen, 'black', bar, bar_outline_w)
+        progress_rect = pg.Rect(
+            bar.topleft + pg.Vector2(bar_outline_w, bar_outline_w), 
+            ((bar.width - (bar_outline_w * 2)) * (progress_percent / 100), bar.height - (bar_outline_w * 2))
+        )
+        pg.draw.rect(self.screen, 'green' if box == self.smelt_box else 'red', progress_rect)
+
+    def run(self, rect_mouse_collide: bool) -> None:
+        self.highlight_surf_when_hovered(rect_mouse_collide)
+        self.update_fuel_status()
+        if not self.render:
+            self.render = rect_mouse_collide and self.mouse.click_states['left']
+            return
+        elif self.keyboard.held_keys[self.key_close_ui]:
+            self.render = False
+            return
+        self.render_interface()
+            
+    def update(self) -> None:
+        self.run(self.machine.rect.collidepoint(self.mouse.world_xy))
+
 
 @dataclass
 class MachineUIHelpers:
